de89eb5c18cfefe35c7ad9bac128dde372e0586b
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-22 20:51:42 -0700

    feat: add bar buttons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { MODULE_NAME } from "./constants.js";
+import { MODULE_NAME, TEMPLATE_PATH } from "./constants.js";
 import "./avbar.css"
 
-Hooks.on('init', async () => {
+Hooks.on('ready', async () => {
     console.log(`${MODULE_NAME} | Initializing ${MODULE_NAME}`);
+    const avbar = $('<div id="avbar"></div>')
+    avbar.html(await renderTemplate(`${TEMPLATE_PATH}/avbar.html`, {}));
+    $('#ui-bottom').append(avbar)
 });
